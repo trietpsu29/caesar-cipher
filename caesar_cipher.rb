5c44a489808128a9b695097a16db241d79aee6d3
Triet Do <112594221+trietpsu29@users.noreply.github.com>
2025-10-28 19:49:23 +0700

    Refactor letter_to_num mapping using range and index

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,32 +1,5 @@
 def caesar_cipher(plain_text,key)
-    letter_to_num = {
-    'a' => 0,
-    'b' => 1,
-    'c' => 2,
-    'd' => 3,
-    'e' => 4,
-    'f' => 5,
-    'g' => 6,
-    'h' => 7,
-    'i' => 8,
-    'j' => 9,
-    'k' => 10,
-    'l' => 11,
-    'm' => 12,
-    'n' => 13,
-    'o' => 14,
-    'p' => 15,
-    'q' => 16,
-    'r' => 17,
-    's' => 18,
-    't' => 19,
-    'u' => 20,
-    'v' => 21,
-    'w' => 22,
-    'x' => 23,
-    'y' => 24,
-    'z' => 25,
-    }
+    letter_to_num = ('a'..'z').to_a.each_with_index.to_h
     num_to_letter = letter_to_num.invert
     plain_text.split('').reduce('') do |res,c|
         if (c >= 'a' && c <= 'z')
