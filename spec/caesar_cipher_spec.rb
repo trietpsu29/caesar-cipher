c842b48792ab3a14f71771c398df140d95132ed9
trietpsu29 <112594221+trietpsu29@users.noreply.github.com>
2026-07-15 12:04:03 +0700

    Add RSpec tests for caesar_cipher

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,1 +1,29 @@
-require_relative 'lib/caesar_cipher'
+require_relative '../lib/caesar_cipher'
+
+RSpec.describe '#caesar_cipher' do
+  it 'encrypts lowercase letters' do
+    text = 'caesar'
+    key = 3
+    expect(caesar_cipher(text, key)).to eq('fdhvdu')
+  end
+  it 'encrypts uppercase letters' do
+    text = 'CAESAR'
+    key = 3
+    expect(caesar_cipher(text, key)).to eq('FDHVDU')
+  end
+  it 'preserves non-letters characters' do
+    text = 'cAe_  S$Ar!!'
+    key = 3
+    expect(caesar_cipher(text, key)).to eq('fDh_  V$Du!!')
+  end
+  it 'handles keys larger than the alphabet size' do
+    text = 'cAe_  S$Ar!!'
+    key = 39
+    expect(caesar_cipher(text, key)).to eq('pNr_  F$Ne!!')
+  end
+  it 'returns the original text when the key is zero' do
+    text = 'cAe_  S$Ar!!'
+    key = 0
+    expect(caesar_cipher(text, key)).to eq('cAe_  S$Ar!!')
+  end
+end
